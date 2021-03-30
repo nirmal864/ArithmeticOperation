7646fb0a1c92b39137598062f32384512b6fab42
Nirmal Randhawa <nirmal_864@hotmail.com>
2021-03-29 22:00:47 -0400

    pass

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -15,8 +15,8 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-	//	assertEquals(expected, actual);
-		fail("must fail");
+		assertEquals(expected, actual);
+	//	fail("must fail");
 	}
  //   @Test
  //   public void testSub()
